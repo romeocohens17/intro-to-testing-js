611d6e5aa04ac9c1db726ccbf93d825cd6198a4e
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-04-20 16:17:45 -0500

    added another test

--- code.js
+++ code.js
@@ -13,7 +13,11 @@ function plusOne(num) {
 function sayHello(input) {
     if (input === "Alex") {
         return "Hello, Alex!"
-    }else {
+    }else if (input === "Pat") {
+        return "Hello, Pat!"
+    }else if (input === "Jane") {
         return "Hello, Jane!"
+    }else {
+        return ""
     }
 }
