2feaa892d6204063aa8914959b31ff94aff66726
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-04-20 19:38:06 -0500

    unit tests for edge cases.

--- tests.js
+++ tests.js
@@ -49,8 +49,8 @@ describe('sayHello' , function(){
     it('should return the string "Hello, Pat!" when executed' , function() {
         expect(sayHello("Pat")).toBe("Hello, Pat!")
     });
-    it('should return the string "Hello, World! when executed' , function(){
-        expect(sayHello(true)).toBe("Hello, World!")
+    it('should return the string "Hello, World!" when executed' , function() {
+        expect(sayHello(Boolean())).toBe("Hello, World!")
     });
 });
 
